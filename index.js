4cadc4b49854ec2f1d23443697f36cd941306df6
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-30 13:30:10 -0400

    form built

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert('js connect')
+// alert('js connect')
+
+const requestGenre = async () => {
+    let req = await fetch('https://binaryjazz.us/wp-json/genrenator/v1/genre/')
+    let res = await req.json()
+    console.log("Your genres, m'lady...", res)
+}
+requestGenre()
