287e12b67c998e7d2d9d9ed9bef9f9ee60399d6e
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-30 13:48:38 -0400

    event listener added, not acting right

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 // alert('js connect')
+let genContainer, genBtn
+genContainer = document.getElementById('generate-container')
+genBtn = document.getElementById('genreNate')
+console.log("Testing, testing, 1, 2,", genContainer)
+
 
 const requestGenre = async () => {
     let req = await fetch('https://binaryjazz.us/wp-json/genrenator/v1/genre/')
     let res = await req.json()
     console.log("Your genres, m'lady...", res)
+    
+    let h3 = document.createElement('h3')
+    h3.innerText = res
+    genContainer.append(h3)
 }
-requestGenre()
+
+
+genBtn.addEventListener('click', () => {
+    requestGenre()
+})
